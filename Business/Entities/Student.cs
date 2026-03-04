d9d445e646add5a8b0fe4943983096878125ec10
GuilhermeLopesBertacini <guilb2005@gmail.com>
2026-03-04 18:18:44 -0300

    feat(student entity): Add Student entity

---
+++ Business/Entities/Student.cs
@@ -0,0 +1,12 @@
+namespace StudentsManagement.Domain.Entities
+{
+  public class Student
+  {
+    public Guid Id { get; private set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string Email { get; private set; }
+    public Guid CourseId { get; private set;}
+    // public Course Course { get; private set; }
+  }
+}
